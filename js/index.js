808a02ab111aab0630d6b0e12466de78fc42c137
Willian_Gasparini <wiiillian21@gmail.com>
2023-12-01 16:07:14 -0300

    Criando Metodos de verifição e adicionar numeros no display

--- js/index.js
+++ js/index.js
@@ -6,8 +6,34 @@ class Calculator {
         this.reset = 0;
     }
 
+    checkLastDigit(input, upperValue, reg) {
+        if((
+            !reg.test(input) &&
+            !reg.test(upperValue.substr(upperValue.length - 1))
+        )) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     btnPress() {
-        console.log(this);
+        const input = this.textContent;
+        const upperValue = calc.upperValue.textContent;
+        // verificações
+        const reg = new RegExp("^\\d+$");
+        
+        if(calc.checkLastDigit(input, upperValue, reg)) {
+            return false;
+        }
+
+
+        if(upperValue === "0") {
+            calc.upperValue.textContent = input;
+        } else {
+            calc.upperValue.textContent += input;
+        }
+
     }
 }
 
