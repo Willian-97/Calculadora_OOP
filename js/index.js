da179f7543a6de1d2c8e708d591dafaa47963389
Willian_Gasparini <wiiillian21@gmail.com>
2023-12-01 15:52:53 -0300

    Inciando JavaScript OOP

--- js/index.js
+++ js/index.js
@@ -1,1 +1,26 @@
-console.log("Ola mundo");
+class Calculator {
+
+    constructor() {
+        this.upperValue = document.querySelector("#upper-number");
+        this.resultValue = document.querySelector("#result-number");;
+        this.reset = 0;
+    }
+
+    btnPress() {
+        console.log(this);
+    }
+}
+
+// start obj
+
+const calc = new Calculator;
+
+// start btns
+
+const buttons = document.querySelectorAll(".btn");
+
+
+// map all buttons
+for(let i = 0; buttons.length > i; i++){
+    buttons[i].addEventListener("click", calc.btnPress);
+}
